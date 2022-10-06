81036d919b7b46e11243d1c1e6a8dd5e335dfb82
shjeon <ohgnu90@korea.ac.kr>
2022-10-06 15:59:19 +0900

    function

--- main.c
+++ main.c
@@ -1,79 +1,42 @@
 /*
 - Author: Seungho Jeon
-- Data: 22.09.29
+- Data: 22.10.06
 - Description: Basic RGP game
+- Patch note:
+	1. function
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+#define TRUE 1
+#define FALSE 0
+
+int player_hp = 100;
+int player_att = 10;
+int player_def = 8;
+
+int monster_hp = 80;
+int monster_att = 10;
+int monster_def = 8;
+
+void print_status(int, int, int, int ,int, int);
+int print_menu();
+int calculate_damage(int, int, int);
+void attack();
+void defense();
+
 int main() {
-
-	int player_hp = 100;
-	int player_att = 10;
-	int player_def = 8;
-
-	int monster_hp = 80;
-	int monster_att = 10;
-	int monster_def = 8;
-
 	srand(time(NULL));
 
 	while (1) {
-		printf("Player status============\n");
-		printf("- HP: %d\n", player_hp);
-		printf("- ATT: %d\n", player_att);
-		printf("- DEF: %d\n", player_def);
-		printf("=========================\n");
-		printf("Monster status============\n");
-		printf("- HP: %d\n", monster_hp);
-		printf("- ATT: %d\n", monster_att);
-		printf("- DEF: %d\n", monster_def);
-		printf("=========================\n");
+		print_status(player_hp, player_att, player_def,
+			monster_hp, monster_att, monster_def);
 
-		printf("Menu=====================\n");
-		printf("1. Attack.\n");
-		printf("2. Defense.\n");
-		printf("3. Run.\n");
-		printf("=========================\n");
+		int choice = print_menu();
 
-		int choice;
-		scanf("%d", &choice);
-
-		if (choice == 1) {
-			int damage = player_att - monster_def;
-			int is_critical = rand() % 2;
-
-			if (is_critical) damage *= 2;
-
-			printf("Hit the monster with damage %d.\n", damage);
-			monster_hp -= damage;
-
-			int cntatt = rand() % 2;
-			if (monster_hp > 0 && cntatt) {
-				printf("Watch out! Monster's counterattack!\n");
-				damage = monster_att - player_def;
-				player_hp -= damage;
-				printf("Got damage %d from the mosnter.\n", damage);
-			}
-		}
-		else if (choice == 2) {
-			int damage = monster_att - player_def;
-			int is_critical = rand() % 2;
-
-			if (is_critical) damage *= 2;
-
-			printf("Got damage %d from the monster.\n", damage);
-			player_hp -= damage;
-
-			int cntatt = rand() % 2;
-			if (player_hp > 0 && cntatt) {
-				printf("Let's counterattack.\n");
-				damage = player_att - monster_def;
-				monster_hp -= damage;
-				printf("Hit the monster with damage %d.\n", damage);
-			}
-		}
+		if (choice == 1) attack();
+		else if (choice == 2) defense();
 		else if (choice == 3) {
 			printf("Bye bye!\n");
 			break;
@@ -94,4 +57,70 @@ int main() {
 	}
 
 	return 0;
+}
+
+void print_status(int player_hp, int player_att, int player_def,
+	int monster_hp, int monster_att, int monster_def) {
+	printf("Player status============\n");
+	printf("- HP: %d\n", player_hp);
+	printf("- ATT: %d\n", player_att);
+	printf("- DEF: %d\n", player_def);
+	printf("=========================\n");
+	printf("Monster status============\n");
+	printf("- HP: %d\n", monster_hp);
+	printf("- ATT: %d\n", monster_att);
+	printf("- DEF: %d\n", monster_def);
+	printf("=========================\n");
+}
+
+int print_menu() {
+	printf("Menu=====================\n");
+	printf("1. Attack.\n");
+	printf("2. Defense.\n");
+	printf("3. Run.\n");
+	printf("=========================\n");
+
+	int choice;
+	scanf("%d", &choice);
+
+	return choice;
+}
+
+int calculate_damage(int att, int def, int do_critical) {
+	int damage = att - def;
+	int is_critical = rand() % 2;
+
+	if (is_critical && do_critical) damage *= 2;
+
+	return damage;
+}
+
+void attack() {
+	int damage = calculate_damage(player_att, monster_def, TRUE);
+
+	printf("Hit the monster with damage %d.\n", damage);
+	monster_hp -= damage;
+
+	int cntatt = rand() % 2;
+	if (monster_hp > 0 && cntatt) {
+		printf("Watch out! Monster's counterattack!\n");
+		damage = calculate_damage(monster_att, player_def, FALSE);
+		player_hp -= damage;
+		printf("Got damage %d from the mosnter.\n", damage);
+	}
+}
+
+void defense() {
+	int damage = calculate_damage(monster_att, player_def, TRUE);
+
+	printf("Got damage %d from the monster.\n", damage);
+	player_hp -= damage;
+
+	int cntatt = rand() % 2;
+	if (player_hp > 0 && cntatt) {
+		printf("Let's counterattack.\n");
+		damage = calculate_damage(player_att, monster_def, FALSE);
+		monster_hp -= damage;
+		printf("Hit the monster with damage %d.\n", damage);
+	}
 }
